e2a155447d1eafc799df8334ec508e8e790bfd77
asadbekme <asadbekme2002@gmail.com>
2026-04-22 12:06:55 +0500

    feat: modify element processing to collect odd numbers and calculate sums

--- python-lesson/tasks/1481.py
+++ python-lesson/tasks/1481.py
@@ -1,10 +1,14 @@
 n, x = map(int, input().split())
 a = list(map(int, input().split()))
+b = []
 for element in a:
     if element % 2 != 0:
-        print(element, end = " ")
+        b.append(element)
     if element == x:
         break
+
+print(*b)
+print(sum(a) - sum(b))
 
     
 
